8030afe9ed4bfb9cf2a4302f30161f8fcd183a6e
sahildps <sahildshetty@gmail.com>
2025-02-16 18:15:33 -0700

    Competitive-coding-7 | Meeting-Rooms-II

---
+++ Meeting-rooms-2.cpp
@@ -0,0 +1,29 @@
+/*
+ * TC : O(nlogn)
+ * SC : O(n)
+ */
+class Solution
+{
+public:
+    int minMeetingRooms(vector<vector<int>> &intervals)
+    {
+        if (intervals.empty() || intervals.size() == 0)
+        {
+            return 0;
+        }
+
+        sort(intervals.begin(), intervals.end());
+        priority_queue<int, vector<int>, greater<int>> minStore;
+
+        for (int i = 0; i < intervals.size(); i++)
+        {
+            if (!minStore.empty() && intervals[i][0] >= minStore.top())
+            {
+                minStore.pop();
+            }
+            minStore.push(intervals[i][1]);
+        }
+
+        return minStore.size();
+    }
+};
